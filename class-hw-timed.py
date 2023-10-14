d71d9df9e152d6fce86d73b38c0343a65915e2ab
jbuckEI <joshua@edgeimpulse.com>
2023-10-14 20:34:51 +0000

    almost working, has dsp and akida

--- class-hw-timed.py
+++ class-hw-timed.py
@@ -4,18 +4,23 @@ import signal
 import time
 import io
 
+#for i2c, gpio, pwms
 import board
 import busio
 import adafruit_adxl34x
 import os
 import pandas as pd
 import numpy as np
-
 import RPi.GPIO as GPIO
 import threading
 
+#for model running
 import akida
 import scipy
+
+#for dsp code
+from spectral_analysis import generate_features
+import numpy as np
 
 
 num_of_samples = 0
@@ -40,6 +45,88 @@ GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.setup(18, GPIO.OUT)
 p = GPIO.PWM(18, 100)
+
+# takes in raw features from the accel in format [x,y,z,x,y,z,etc]
+def dsp(features):
+
+    processed_features = []
+
+    # Parameters for generate_features
+
+    ## The first section are parameters that apply for any DSP block
+    # Version of the implementation.  If you want the latest, look into parameters.json, and use the value of latestImplementationVersion
+    implementation_version = 4 # 4 is latest versions
+    draw_graphs = False # For testing from script, disable graphing to improve speed
+
+    # This is where you want to paste in your test sample.  This can be taken from studio
+    #  For example, the below sample is from https://studio.edgeimpulse.com/public/223682/latest/classification#load-sample-269491318
+    #  It was copied by clicking on the copy icon next to "Raw features"
+    #  It is 3 axis accelerometer data, with 62.5Hz sampling frequency
+    #  Data should be formatted as a single flat list, regardless of the number of axes/channels
+    raw_data = np.array(features)
+
+    axes = ['x', 'y', 'z'] # Axes names.  Can be any labels, but the length naturally must match the number of channels in raw data
+    sampling_freq = 100 # Sampling frequency of the data.  Ignored for images
+
+    # Below here are parameters that are specific to the spectral analysis DSP block. These are set to the defaults
+    scale_axes = 1 # Scale your data if desired
+    input_decimation_ratio = 1 # Decimation ratio.  See /spectral_analysis/paramters.json:31 for valid ratios
+    filter_type = 'none' # Filter type.  String : low, high, or none
+    filter_cutoff = 0 # Cutoff frequency if filtering is chosen.  Ignored if filter_type is 'none'
+    filter_order = 0 # Filter order.  Ignored if filter_type is 'none'.  2, 4, 6, or 8 is valid otherwise
+    analysis_type = 'FFT' # Analysis type.  String : FFT, wavelet
+
+    # The following parameters only apply to FFT analysis type.  Even if you choose wavelet analysis, these parameters still need dummy values
+    fft_length = 16 # Size of FFT to perform.  Should be power of 2 >-= 16 and <= 4096
+
+    # Deprecated parameters.  Only applies to version 1, maintained for backwards compatibility
+    spectral_peaks_count = 0 # Deprecated parameter.  Only applies to version 1, maintained for backwards compatibility
+    spectral_peaks_threshold = 0 # Deprecated parameter.  Only applies to version 1, maintained for backwards compatibility
+    spectral_power_edges = "0" # Deprecated parameter.  Only applies to version 1, maintained for backwards compatibility
+
+    # Current FFT parameters
+    do_log = True # Take the log of the spectral powers from the FFT frames
+    do_fft_overlap = True # Overlap FFT frames by 50%.  If false, no overlap
+    extra_low_freq = False # This will decimate the input window by 10 and perform another FFT on the decimated window.
+                        # This is useful to extract low frequency data.  The features will be appended to the normal FFT features
+
+    # These parameters only apply to Wavelet analysis type.  Even if you choose FFT analysis, these parameters still need dummy values
+    wavelet_level = 1 # Level of wavelet decomposition
+    wavelet = "" # Wavelet kernel to use
+
+
+    output = generate_features(implementation_version, draw_graphs, raw_data, axes, sampling_freq, scale_axes, input_decimation_ratio,
+                        filter_type, filter_cutoff, filter_order, analysis_type, fft_length, spectral_peaks_count,
+                        spectral_peaks_threshold, spectral_power_edges, do_log, do_fft_overlap,
+                        wavelet_level, wavelet, extra_low_freq)
+
+    # Return dictionary, as defined in code
+        # return {
+        #     'features': List of output features
+        #     'graphs': Dictionary of graphs
+        #     'labels': Names of the features
+        #     'fft_used': Array showing which FFT sizes were used.  Helpful for optimzing embedded DSP code
+        #     'output_config': information useful for correctly configuring the learn block in Studio
+        # }
+
+    print(f'Processed features are: ')
+    #print('Feature name, value')
+    idx = 0
+    for axis in axes:
+        #print(f'\nFeatures for axis: {axis}')
+        for label in output['labels']:
+            #print(f'{label: <40}: {output["features"][idx]}')
+            processed_features.append(output["features"][idx])
+    
+
+    print(processed_features)
+    print(type(processed_features))
+    
+    processed_features_np = np.array(processed_features)
+    print(type(processed_features_np))
+
+    return processed_features
+
 
 def akida_model_inference(model, processed_features):
 
@@ -91,9 +178,6 @@ def signal_handler(sig, frame):
     print('Interrupted')
     p.stop()
     GPIO.cleanup()
-
-    if (runner):
-        runner.stop()
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
@@ -119,7 +203,11 @@ def main(argv):
         sys.exit(2)
 
     model = args[0]
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    modelfile = os.path.join(dir_path, model)
+    #print('MODEL: ' + modelfile)
 
+    #start pws
     p.start(50)
     #wait for data collection to finish
     done_collecting.wait()
@@ -127,22 +215,14 @@ def main(argv):
     done_collecting.clear()
 
     features = data
+    #print(len(features))
+    #print(features)
 
-    print(len(features))
+    #run the dsp
+    processed_features = dsp(features)
 
-    print(features)
-
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    modelfile = os.path.join(dir_path, model)
-
-    print('MODEL: ' + modelfile)
-
-    #fixme: need to add python DSP code
-    # here is a hard coded inference with processed features
-
-    processed_features = np.array([0.1467, 0.2941, 1.3105, 2.0280, 2.8643, 1.5801, 1.5660, 1.4363, 1.3001, 1.1117, 1.1817, 1.2346, 0.6545, 0.0479, 0.5384, 0.6327, 1.4070, 0.5814, 2.4727, 2.2234, 2.1496, 2.2316, 1.7698, 2.1855, 2.2644, 1.6087, 0.0852, 0.3664, 0.4464, 1.1865, 0.8909, 2.2625, 1.9151, 1.8818, 1.6190, 1.6399, 1.7571, 1.6380, 1.3321])
+    #run the model on akida
     akida_model = akida.Model(modelfile)
-
     predictions = akida_model_inference(akida_model, processed_features)
 
     print(predictions)
@@ -150,10 +230,13 @@ def main(argv):
 
 if __name__ == '__main__':
 
+    #startup the thread
     done_collecting = threading.Event()
     done_collecting.clear()
 
+    #set the gpio pin input to trigger an interrupt
     GPIO.add_event_detect(16, GPIO.RISING, 
         callback=data_acq_callback, bouncetime=10)
 
+    #send args to main function
     main(sys.argv[1:])
