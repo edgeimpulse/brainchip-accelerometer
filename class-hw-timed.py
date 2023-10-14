2a777562abebe0521f7fef6d9092ac99fa0fd78b
Joshua Buck <joshua@edgeimpulse.com>
2023-10-14 13:57:35 -0500

    adding how to use fbz

--- class-hw-timed.py
+++ class-hw-timed.py
@@ -22,6 +22,8 @@ import sys
 import RPi.GPIO as GPIO
 import threading
 
+from akida import devices
+
 runner = None
 
 num_of_samples = 0
@@ -46,6 +48,23 @@ GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.setup(18, GPIO.OUT)
 p = GPIO.PWM(18, 100)
+
+def akida_model_inference(model, processed_features):
+
+    scaling_factor = 15 / np.max(processed_features)
+    # Convert to uint8
+    processed_features_uint8 = np.uint8(processed_features * scaling_factor)
+
+    # Reshape to model input shape
+    input_shape = (1,) + tuple(model.input_shape)  # Assuming model.input_shape returns (39,)
+    inputs = processed_features_uint8.reshape(input_shape)
+
+    # Perform inference
+    device = devices()[0]
+    model.map(device)
+    # model.summary()
+    results = model.predict(inputs)
+    return results
 
 def data_acq_callback(channel):
     global num_of_samples
@@ -88,7 +107,7 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 def help():
-    print('python classify.py <path_to_model.eim>')
+    print('python3 claa-hw-time.py <path-to-akida-model.fbz>')
 
 def main(argv):
     try:
@@ -127,22 +146,10 @@ def main(argv):
     print('MODEL: ' + modelfile)
 
     #add pythong dsp code
-    #pass to akida libraries to for inferencing
+    processed_features = np.array([0.1467, 0.2941, 1.3105, 2.0280, 2.8643, 1.5801, 1.5660, 1.4363, 1.3001, 1.1117, 1.1817, 1.2346, 0.6545, 0.0479, 0.5384, 0.6327, 1.4070, 0.5814, 2.4727, 2.2234, 2.1496, 2.2316, 1.7698, 2.1855, 2.2644, 1.6087, 0.0852, 0.3664, 0.4464, 1.1865, 0.8909, 2.2625, 1.9151, 1.8818, 1.6190, 1.6399, 1.7571, 1.6380, 1.3321])
+    akida_model = akida.Model(modelfile)
 
-    runner = ImpulseRunner(modelfile)
-    try:
-        model_info = runner.init()
-        print('Loaded runner for "' + model_info['project']['owner'] + ' / ' + model_info['project']['name'] + '"')
-
-        res = runner.classify(features)
-        print("classification:")
-        print(res["result"])
-        print("timing:")
-        print(res["timing"])
-
-    finally:
-        if (runner):
-            runner.stop()
+    predictions = akida_model_inference(akida_model, processed_features)
 
 if __name__ == '__main__':
 
